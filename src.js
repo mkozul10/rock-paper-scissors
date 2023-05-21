3549fd94ca4f5439c4785d7ff5f902db48823c3e
Mario <mariokozul24@gmail.com>
2023-05-21 14:42:17 +0200

    added playRound() function

--- src.js
+++ src.js
@@ -19,3 +19,24 @@ function getPlayerChoice(){
     }
     return choice;
 }
+function playRound(PlayerChoice,ComputerChoice){
+    switch(PlayerChoice){
+        case CHOICES[0]:
+            if(ComputerChoice===CHOICES[0]) return 'TIE, rock vs rock';
+            else if(ComputerChoice===CHOICES[1]) return 'YOU LOST, paper covers rock';
+            else return 'YOU WON, rock crushes scissors';
+            break;
+        case CHOICES[1]:
+            if(ComputerChoice===CHOICES[0]) return 'YOU WON, paper covers rock';
+            else if(ComputerChoice===CHOICES[1]) return 'TIE, paper vs paper';
+            else return 'YOU LOST, scissors cut paper';
+            break;
+        case CHOICES[2]:
+            if(ComputerChoice===CHOICES[0]) return 'YOU LOST, rock crushes scissors';
+            else if(ComputerChoice===CHOICES[1]) return 'YOU WON, scissors cut paper';
+            else return 'TIE, scissors vs scissors';
+            break;
+        default:
+            break;
+    }
+}
